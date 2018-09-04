c33fb80f378fabd558be75f437fb1eabfbe8bde3
NMahler <nicolasmahler@me.com>
2018-09-04 09:53:15 -0400

    added starting project

---
+++ robofriends/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import App from './containers/App';
+import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
+registerServiceWorker();
